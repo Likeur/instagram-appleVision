369305cb4dd8ebd68867e9bbcfad7c7ebbc9a7f0
Likeur <jacksimba28@gmail.com>
2024-01-27 00:43:42 +0200

    adding message Liste and profil components

---
+++ src/app/components/message-list/message-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-list.component.html',
+  styles: ``
+})
+export class MessageListComponent {
+
+}
---
+++ src/app/components/profil/profil.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profil',
+  standalone: true,
+  imports: [],
+  templateUrl: './profil.component.html',
+  styles: ``
+})
+export class ProfilComponent {
+
+}
