178a8202d4ccab4c615ff173eab816ec44d6c0d1
Likeur <jacksimba28@gmail.com>
2024-01-26 04:01:26 +0200

    working on sidebarMenu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        title:'instagram-appleVision',
+        loadComponent: () => import('./components/home/home.component')
+    },
+    {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+    }
+];
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+})
+export default class HomeComponent {
+
+  imgLink = '../../../assets/img/fond2.jpg'
+}
