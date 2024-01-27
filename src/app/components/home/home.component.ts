c78b985d6045b254f6775bf4f89c95e2833ea7d7
Likeur <jacksimba28@gmail.com>
2024-01-27 02:35:16 +0200

    working on profile

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProfilComponent } from '../profil/profil.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProfilComponent],
   templateUrl: './home.component.html',
 })
 export default class HomeComponent {
