3554bfe533a87b283be3dded53f0d9b226420020
Likeur <jacksimba28@gmail.com>
2024-02-01 03:00:07 +0200

    messagelist finish

--- src/app/components/message-list/message-list.component.ts
+++ src/app/components/message-list/message-list.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-message-list',
   standalone: true,
   imports: [],
   templateUrl: './message-list.component.html',
-  styles: ``
 })
 export class MessageListComponent {
 
+  openPosttl = gsap.timeline({
+    defaults:{
+      duration:1,
+      ease:'power4.inOut'
+    }
+  })
+
+  openMessageview(){
+    this.openPosttl.to('#main_box',{
+      scale:0.5,
+    }).to('#viewpostBoxewrapper',{
+      display:'grid'
+    }, "<").to('#viewpostBoxe',{
+      scale:1
+    }, "<")
+  }
 }
