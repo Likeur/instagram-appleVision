0494b9711d61cc7741e8b1043aad729389d69228
Likeur <jacksimba28@gmail.com>
2024-02-01 02:11:25 +0200

    working on animation

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProfilComponent } from '../profil/profil.component';
 import { Draggable } from "gsap/Draggable";
 import gsap from 'gsap';
+import { isFormGroup } from '@angular/forms';
 
 gsap.registerPlugin(Draggable)
 @Component({
@@ -16,6 +17,7 @@ export default class HomeComponent implements OnInit {
 
   isLikeClicked = false
   isBookMarkCliked = false
+  numberOfLike = 1232
 
   ngOnInit(): void {
    Draggable.create('#storiesBox', {
@@ -33,6 +35,34 @@ export default class HomeComponent implements OnInit {
   }
   likeClicked(){
     this.isLikeClicked = !this.isLikeClicked
+
+    const likeTl = gsap.timeline({
+      defaults:{
+        duration:1,
+        ease:'power4.inOut'
+      }
+    })
+
+    likeTl.to('.likeRed',{
+      scale: 1,
+      stagger:0.2
+    }).to('.likeRed',{
+      y:30,
+      opacity:0,
+      scale:0
+    })
+
+    likeTl.paused()
+
+    if(this.isLikeClicked){
+      likeTl.play()
+
+      this.numberOfLike += 1
+    }else if(!this.isLikeClicked){
+      this.numberOfLike -=1
+      likeTl.restart()
+    }
+    
   }
   bookmarkClicked(){
     this.isBookMarkCliked = !this.isBookMarkCliked
@@ -53,5 +83,25 @@ export default class HomeComponent implements OnInit {
     }, "<").to('#viewpostBoxe',{
       scale:0
     }, "<")
+  }
+
+  closeProfileBox(){
+    gsap.to('#profilBox',{
+      x:60,
+      opacity:0,
+      display:'none'
+    })
+  }
+
+  openProfileBox(){
+    gsap.to('#profilBox',{
+      x:0,
+      opacity:1,
+      display:'block'
+    })
+  }
+
+  onLike(){
+    
   }
 }
